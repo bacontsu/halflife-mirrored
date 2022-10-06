128c6bf7fd404776bfa592e889422c67f00c276b
Bacontsu <62763382+bacontsu@users.noreply.github.com>
2022-10-06 12:48:13 +0700

    Update hud.cpp

--- cl_dll/hud.cpp
+++ cl_dll/hud.cpp
@@ -321,6 +321,7 @@ void CHud::Init()
 	CVAR_CREATE("hud_classautokill", "1", FCVAR_ARCHIVE | FCVAR_USERINFO); // controls whether or not to suicide immediately on TF class switch
 	CVAR_CREATE("hud_takesshots", "0", FCVAR_ARCHIVE);					   // controls whether or not to automatically take screenshots at the end of a round
 
+	CVAR_CREATE("cl_righthand", "1", FCVAR_ARCHIVE);
 
 	m_iLogo = 0;
 	m_iFOV = 0;
